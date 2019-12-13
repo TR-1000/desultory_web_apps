9144e4991df79d2240bdcbcea44e7da2a4221729
TR-1000 <t.l.ross@outlook.com>
2019-12-12 23:24:15 -0600

    Saving progress

--- hash-app/hashit/hashes/models.py
+++ hash-app/hashit/hashes/models.py
@@ -5,3 +5,17 @@ from django.db import models
 class Hash(models.Model):
     text = models.TextField()
     hash = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.text
+        return self.hash
+
+
+
+class Url(models.Model):
+    url_address = models.URLField(max_length=2083)
+    url_hash = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.url_address
+        return self.url_hash
