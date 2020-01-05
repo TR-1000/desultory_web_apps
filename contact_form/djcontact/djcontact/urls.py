a094cf5bc14a86eb12a29236081d36f016fdb50b
TR-1000 <t.l.ross@outlook.com>
2020-01-04 21:45:11 -0600

    Removed old projects

--- contact_form/djcontact/djcontact/urls.py
+++
@@ -1,8 +0,0 @@
-# djcontact/urls.py
-from django.contrib import admin
-from django.urls import path, include # new
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('sendemailapp.urls')), # new
-]
--- contact_form/djcontact/sendemailapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SendemailappConfig(AppConfig):
-    name = 'sendemailapp'
