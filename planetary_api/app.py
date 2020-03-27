712fb0a2df096ac0add2952adb9e958c67b8ecd9
TR-1000 <t.l.ross@outlook.com>
2020-03-27 09:07:08 -0500

    Added delete route

--- planetary_api/app.py
+++ planetary_api/app.py
@@ -179,6 +179,19 @@ def update_planet():
         return jsonify(Message='That planet doesn\'t exist'), 404
 
 
+@app.route('/remove_planet/<int:planet_id>', methods=['DELETE'])
+@jwt_required
+def remove_planet(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message='You\'ve deleted a planet'), 202
+    else:
+        return jsonify(message='That planet does not exist'),404
+
+
+
 
 @app.route('/register', methods=['POST'])
 def register():
