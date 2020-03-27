1859e2edc90a51b9c8a5e7e13b47a06d52439542
TR-1000 <t.l.ross@outlook.com>
2020-03-27 08:54:21 -0500

    Added update route

--- planetary_api/app.py
+++ planetary_api/app.py
@@ -162,10 +162,21 @@ def add_planet():
 
 
 @app.route('/update_planet', methods=['PUT'])
+@jwt_required
 def update_planet():
     planet_id = int(request.form['planet_id'])
     planet = Planet.query.filter_by(planet_id=planet_id).first()
-    ###### CONTINUE HERE ######      
+    if planet:
+        planet.planet_name = request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message='You updated a planet'), 202
+    else:
+        return jsonify(Message='That planet doesn\'t exist'), 404
 
 
 
