4aeda9ad0bdd103c6df670da6efd0dbc6a838f3b
TR-1000 <t.l.ross@outlook.com>
2023-05-11 19:18:19 +0000

    initial spring boot commit

---
+++ sprung/src/main/java/com/tr1000/sprung/SprungApplication.java
@@ -0,0 +1,13 @@
+package com.tr1000.sprung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprungApplication.class, args);
+	}
+
+}
---
+++ sprung/src/test/java/com/tr1000/sprung/SprungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr1000.sprung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
